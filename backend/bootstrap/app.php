39cd5f65dc62cf8ce8eedc8bc7ae28f4ad7e281b
jair-chaguay <jchaguay@espol.edu.ec>
2025-10-20 13:24:25 -0500

    Update frontend and api sanctum

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->appendToGroup('api', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
         
     })
     ->withExceptions(function (Exceptions $exceptions): void {
