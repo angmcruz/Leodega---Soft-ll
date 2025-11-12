9692383279b9b0c04e6d43c8c656d5d8dd73c35b
jair-chaguay <jchaguay@espol.edu.ec>
2025-11-12 12:40:24 -0500

    nan

--- frontend/src/Dashboard/BodegasArrendador.tsx
+++ frontend/src/Dashboard/BodegasArrendador.tsx
@@ -168,7 +168,7 @@ const BodegasArrendador = () => {
       ) : bodegasOrdenadas.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {bodegasOrdenadas.map((bodega) => (
-            <BodegaCard key={bodega.id} {...bodega} 
+            <BodegaCard key={bodega.id} {...bodega}
             storePrices={bodega.storePrices}/>
           ))}
         </div>
