e362558d9ddcf71f45141874b0e3886e390e04ae
angmcruz <108002776+angmcruz@users.noreply.github.com>
2025-11-22 00:53:32 -0500

    HAY FE

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -21,8 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustHosts([ // se cambia a hosting cuando haya dominio real
             '127\.0\.0\.1',
             'localhost',
+            'leodegafront\.vercel\.app',
+            'leodega-soft-ll-production-a8de\.up\.railway\.app'
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
