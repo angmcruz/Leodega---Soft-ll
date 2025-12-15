874708fcc19ad0e3bc12cda9f4f31a92c7f44628
jair-chaguay <jchaguay@espol.edu.ec>
2025-12-14 19:47:32 -0500

    language

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import "./i18n";
-import { loadTranslations } from "./loadTranslations";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const bootstrap = async () => {
-  await loadTranslations(); 
 
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-};
-
-bootstrap();
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
